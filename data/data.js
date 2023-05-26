c86959ecfde289ff295d3f122e3c18867cc8ef40
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-05-26 18:02:24 -0300

    clientes

---
+++ data/data.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: "Juan",
+      last: "perez",
+    },
+    total: 2500,
+    status: "On Hold",
+    method: "PayPal",
+    date: "Hace 15 minutos",
+  },
+  {
+    id: 1,
+    name: {
+      first: "Roberto",
+      last: "Sanchez",
+    },
+    total: 1500,
+    status: "Processing",
+    method: "PayPal",
+    date: "Hace 20 minutos",
+  },
+  {
+    id: 1,
+    name: {
+      first: "Lio",
+      last: "Messi",
+    },
+    total: 1500,
+    status: "Processing",
+    method: "PayPal",
+    date: "Hace 5 minutos",
+  },
+  {
+    id: 1,
+    name: {
+      first: "Charly",
+      last: "García",
+    },
+    total: 1000,
+    status: "Processing",
+    method: "PayPal",
+    date: "Hace 5 minutos",
+  },
+];
